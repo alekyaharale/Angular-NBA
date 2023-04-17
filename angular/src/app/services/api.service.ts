7ec6c9cdf94c0d2842f35de810bb2e48a1334daa
alekyaharale <alekhyaharale1782@gmail.com>
2023-04-17 11:33:54 +0530

    env file changes

--- angular/src/app/services/api.service.ts
+++ angular/src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { MatchesApiResponse, TeamsApiResponse, teamData } from './data.service';
+import { environment } from 'src/environment/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +10,8 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
   header = new HttpHeaders({
-    'X-RapidAPI-Key': '2QMXSehDLSmshDmRQcKUIAiQjIZAp1UvKUrjsnewgqSP6F5oBX',
-    'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+    'X-RapidAPI-Key': environment.API_KEY,
+    'X-RapidAPI-Host': environment.API_HOST
   });
 
   CUSTOM_API = 'https://free-nba.p.rapidapi.com';
@@ -22,7 +23,6 @@ export class ApiService {
 
   getMatches(teamId: number, dates: string[]):Observable<MatchesApiResponse> {
     let param = new HttpParams()
-      .append('per_page', 100)
       .append('team_ids[]', teamId)
 
     dates.forEach((date: string) => {
